9637ea8be2db10ecb1c2f9c97dd305f210df2ba6
Anant Bhardwaj <anantb@voicex.csail.mit.edu>
2012-10-20 14:53:53 -0400

    hooking django

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,8 @@
 # This file contains python variables that configure Lamson for email processing.
+import sys
 import logging
+
+sys.path.append('./webapp')
 
 # You may add additional parameters such as `username' and `password' if your
 # relay server requires authentication, `starttls' (boolean) or `ssl' (boolean)
@@ -13,5 +16,8 @@ handlers = ['app.handlers.slow_distribute', 'lamson.handlers.log']
 router_defaults = {'host': 'localhost'}
 
 template_config = {'dir': 'app', 'module': 'templates'}
+
+# hook django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "browser.settings")
 
 # the config/boot.py will turn these values into variables set in settings
