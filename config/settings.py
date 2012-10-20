c486a67415d13a726480602bdeaa88251c13ebbb
Anant Bhardwaj <anantb@voicex.csail.mit.edu>
2012-10-20 17:01:21 -0400

    all bugs fixed

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,8 @@
 # This file contains python variables that configure Lamson for email processing.
-#import sys, os
+import sys, os
 import logging
+sys.path.append('./app')
 
-#sys.path.append('server')
 
 # You may add additional parameters such as `username' and `password' if your
 # relay server requires authentication, `starttls' (boolean) or `ssl' (boolean)
@@ -11,13 +11,13 @@ relay_config = {'host': 'localhost', 'port': 8825}
 
 receiver_config = {'host': 'slow.csail.mit.edu', 'port': 25}
 
-handlers = ['server.handlers.slow_distribute', 'lamson.handlers.log']
+handlers = ['app.handlers.slow_distribute', 'lamson.handlers.log']
 
 router_defaults = {'host': 'slow.csail.mit.edu'}
 
-template_config = {'dir': 'server', 'module': 'templates'}
+template_config = {'dir': 'app', 'module': 'templates'}
 
 # hook django
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "browser.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "browser.settings")
 
 # the config/boot.py will turn these values into variables set in settings
