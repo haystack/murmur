828cdf8f0de5fd7c3c1b5fcebb5a2c632986327a
Anant Bhardwaj <anantb@voicex.csail.mit.edu>
2012-10-22 15:55:46 -0400

    all required models

--- modules/handler/models.py
+++ modules/handler/models.py
@@ -8,8 +8,8 @@ Slow_Email Models
 '''
 
 class Post(models.Model):
-	id = models.AutoField(primary_key=True)
-	from_addr = models.CharField(max_length=50)
+	id = models.CharField(max_length=50, primary_key=True)
+	from_email = models.CharField(max_length=50)
 	message = models.TextField()
 	timestamp = models.DateTimeField(auto_now=True)
 
@@ -19,10 +19,36 @@ class Post(models.Model):
 	class Meta:
 		db_table = "posts"
 
+class Group(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=20)
+	status = models.BooleanField()
+	timestamp = models.DateTimeField(auto_now=True)
+	def __unicode__(self):
+		return self.name
+
+	class Meta:
+		db_table = "groups"
+
+
+
+
+class User(models.Model):
+        id = models.AutoField(primary_key=True)
+        email = models.CharField(max_length=50) 
+        group = models.ForeignKey('Group')
+	admin = models.BooleanField()
+	status = models.BooleanField()
+        def __unicode__(self):
+                return self.name
+
+        class Meta:
+                db_table = "users"
+
 
 class Active_Members(models.Model):
 	id = models.AutoField(primary_key=True)
-	message = models.ForeignKey('post')
+	message = models.ForeignKey('Post')
 	active_list = models.TextField()
 	timestamp = models.DateTimeField(auto_now=True)
 	def __unicode__(self):
@@ -32,3 +58,15 @@ class Active_Members(models.Model):
 		db_table = "active_members"
 
 
+
+class Reply(models.Model):
+        id = models.AutoField(primary_key=True)
+	from_email = models.CharField(max_length=50)
+        message = models.ForeignKey('Post')
+        reply = models.TextField()
+        timestamp = models.DateTimeField(auto_now=True)
+        def __unicode__(self):
+                return self.name
+
+        class Meta:
+                db_table = "replies"
