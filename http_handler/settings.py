0496c737f87032fb4da06fa4652f94dcbd5810fa
Anant Bhardwaj <anantb@csail.mit.edu>
2012-11-13 15:12:25 -0500

    make the db name mailx

--- http_handler/settings.py
+++ http_handler/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'slow_email',                      # Or path to database file if using sqlite3.
+        'NAME': 'mailx',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'postgres',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
