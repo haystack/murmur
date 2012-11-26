39a8d84d47d3f95a62049dadc3b79d4e29f3fd46
Anant Bhardwaj <anantb@csail.mit.edu>
2012-11-25 20:12:03 -0500

    separate cookie for mailx

--- http_handler/settings.py
+++ http_handler/settings.py
@@ -69,6 +69,9 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
+
+SESSION_COOKIE_PATH = '/mailx'
+
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
