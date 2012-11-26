7a83fb3a328dbf2333b5699d48a8cfabc98db4bf
Anant Bhardwaj <anantb@csail.mit.edu>
2012-11-25 19:53:50 -0500

    move everything to browser

--- browser/urls.py
+++ browser/urls.py
@@ -6,4 +6,15 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
      url(r'^$', 'browser.views.index'),
+	 url(r'^list_groups', 'browser.views.list_groups'),
+	 url(r'^create_group', 'browser.views.create_group'),
+	 url(r'^activate_group', 'browser.views.activate_group'),
+	 url(r'^deactivate_group', 'browser.views.deactivate_group'),
+	 url(r'^subscribe_group', 'browser.views.subscribe_group'),
+	 url(r'^unsubscribe_group', 'browser.views.unsubscribe_group'),
+	 url(r'^group_info', 'browser.views.group_info'),
+	 url(r'^insert_post', 'browser.views.insert_post'), 
+	 url(r'^insert_reply', 'browser.views.insert_reply'),
+	 url(r'^follow_post', 'browser.views.follow_post'),
+	 url(r'^unfollow_post', 'browser.views.unfollow_post'),
 )
--- browser/views.py
+++ browser/views.py
@@ -2,19 +2,123 @@ from django.http import *
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-
+import engine.main
+from engine.msg_codes import *
+import json
 
 '''
 @author: Anant Bhardwaj
 @date: Nov 9, 2012
 
-MailX Browser
+MailX Web Handler
 '''
 
 
 def index(request):
 	return render_to_response("index.html")
 
+
+def list_groups(request):
+	try:
+		res = engine.main.list_groups()
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+
+
+
+def create_group(request):
+	try:
+		res = engine.main.create_group(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+
+
+
+
+def activate_group(request):
+	try:
+		res = engine.main.activate_group(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+
+
+
+
+def deactivate_group(request):
+	try:
+		res = engine.main.deactivate_group(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+
+
+
+
+def subscribe_group(request):
+	try:
+		res = engine.main.subscribe_group(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+	
+
+
+
+def unsubscribe_group(request):
+	try:
+		res = engine.main.unsubscribe_group(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+
+
+
+def group_info(request):
+	try:
+		res = engine.main.group_info(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+
+
+
+
+def insert_post(request):
+	try:
+		res = engine.main.insert_post(request.POST['group_name'], request.POST['message'], request.POST['poster_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+	
+
+
+def insert_reply(request):
+	try:
+		res = engine.main.insert_reply(request.POST['group_name'], request.POST['message'], request.POST['poster_email'], request.POST['post_id'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+	
+
+
+def follow_post(request):
+	try:
+		res = engine.main.follow_post(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
+
+
+
+def unfollow_post(request):
+	try:
+		res = engine.main.unfollow_post(request.POST['group_name'], request.POST['requester_email'])
+		return HttpResponse(json.dumps(res), mimetype="application/json")
+	except:
+		return HttpResponse(request_error, mimetype="application/json")
 
 
 
