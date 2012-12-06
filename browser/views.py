47012b449268c79a01466ec9a94e8ec9c4c4867e
Anant Bhardwaj <anantb@csail.mit.edu>
2012-12-06 14:40:02 -0500

    remove mail response

--- browser/views.py
+++ browser/views.py
@@ -2,9 +2,10 @@ from django.http import *
 from django.shortcuts import render_to_response
 import engine.main
 from engine.msg_codes import *
-from lamson.mail import MailResponse
-from config.settings import relay
-from smtp_handler.main import *
+
+#from lamson.mail import MailResponse
+#from config.settings import relay
+#from smtp_handler.main import *
 from django.core.context_processors import csrf
 import json
 
@@ -164,6 +165,7 @@ def insert_post(request):
 	try:
 		res = engine.main.insert_post(request.POST['group_name'], request.POST['subject'],  request.POST['msg_text'], request.POST['poster_email'])
 		res.update({'user': request.session[SESSION_KEY]})
+		'''
 		msg_id = res['msg_id']
 		thread_id = res['thread_id']
 		to_send =  res['recipients']
@@ -174,6 +176,7 @@ def insert_post(request):
 		logging.debug('TO LIST: ' + str(to_send))
 		if(len(to_send)>0):
 			relay.deliver(mail, To = to_send)
+		'''
 		return HttpResponse(json.dumps(res), mimetype="application/json")
 	except:
 		return HttpResponse(request_error, mimetype="application/json")
