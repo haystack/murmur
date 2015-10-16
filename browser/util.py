b5d520587c81cbc96c3d68069a6c23ddddb47eda
Amy Zhang <amy.xian.zhang@gmail.com>
2015-10-16 02:47:30 -0400

    fix load groups

--- browser/util.py
+++ browser/util.py
@@ -53,26 +53,24 @@ ALLOWED_ATTRIBUTES = {
 ALLOWED_STYLES = ['border-style', 'border-width', 'float', 'height', 'margin', 'width']
 
 
-def load_groups(request, group_names, user, group_name=None):
+def load_groups(request, groups, user, group_name=None):
     
     if group_name:
         active_group = group_name
     else:
-        if request:
-            active_group = request.GET.get('group_name')
-        else:
-            active_group = None
+        active_group = request.GET.get('group_name')
         
+    names = [g['name'] for g in groups]
+    
     if active_group:
         active_group = {'name': active_group,
                         'active': True}
-    elif request.session.get('active_group') in group_names:
+    elif request.session.get('active_group') in names:
         active_group = {'name': request.session.get('active_group'),
                         'active': True}
-        
-    elif len(group_names) > 0:
-        active_group = {'name': group_names[0],
-                        'active': True}
+    elif len(groups) > 0:
+        active_group = groups[0]
+        active_group['active'] = True
     else:
         active_group = {'name': 'No Groups Yet',
                         'active': False}
