dd7a4f0ad76a1a0d9afcd32101ef0bcd3d9c7371
oliverjd <oliver@dunkley.me>
2017-01-12 10:31:26 -0500

    Update

--- engine/main.py
+++ engine/main.py
@@ -1057,8 +1057,8 @@ def insert_reply(group_name, subject, message_text, user, sender_addr, msg_id, f
 
 def upvote(post_id, email=None, user=None):
 	p = Post.objects.get(id=int(post_id))
-	body = "Your post, \"" + p.subject + "\" was upvoted by " + user.email
-	mail = MailResponse(From = 'no_reply@murmur-dev.csail.mit.edu', To = p.poster_email, Subject = '['+p.group.name+'] Your post was upvoted by '+user.email, Body = body)
+	#body = "Your post, \"" + p.subject + "\" was upvoted by " + user.email
+	mail = MailResponse(From = 'no_reply@murmur-dev.csail.mit.edu', To = p.poster_email, Subject = '['+p.group.name+'] Your post was upvoted by '+user.email, Body = '')
 	relay_mailer.deliver(mail, To = [p.poster_email])
 
 	res = {'status':False}
