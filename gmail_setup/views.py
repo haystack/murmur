f9a5b7f444c48b4906a2a553075308f08441272f
oliverjd <oliver@dunkley.me>
2017-01-23 18:20:18 -0500

    Updated redirect URI

--- gmail_setup/views.py
+++ gmail_setup/views.py
@@ -59,13 +59,14 @@ def index(request):
 def auth(request):
     # use the first REDIRECT_URI if you are developing your app locally, and the second in production
     #REDIRECT_URI = 'http://localhost:8000/gmail_setup/callback'
-    REDIRECT_URI = "http://%s%s" % (BASE_URL, reverse("oauth2:return"))
+    REDIRECT_URI = "http://%s%s" % (BASE_URL, reverse("oauth2:return")) 
     #REDIRECT_URI = 'https://' + BASE_URL + '/gmail_setup/callback'
     FLOW = flow_from_clientsecrets(
         CLIENT_SECRETS,
         scope='https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.settings.basic',
         redirect_uri=REDIRECT_URI
     )
+    
     user = request.user
     storage = Storage(CredentialsModel, 'id', user, 'credential')
     credential = storage.get()
