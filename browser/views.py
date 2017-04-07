9c586c2400944cb8b26021582d53e92eb7517fa5
Kaitlin Mahar <kaitlinmahar@gmail.com>
2017-04-07 17:50:03 -0400

    fix add_members problem

--- browser/views.py
+++ browser/views.py
@@ -1148,7 +1148,7 @@ def subscribe_get(request):
 	elif email_param:
 
 		response.update({
-			'res' : engine.main.add_members(group_name, email_param, None), 
+			'res' : engine.main.add_members(group_name, email_param, False, None), 
 			'email' : email_param,
 			});
 
