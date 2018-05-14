592ef1944cf0a6c317a5b8a5764adb975d0f0561
Soya <soya@mit.edu>
2018-05-14 15:28:00 -0400

    Import DoNotSendList at view

--- browser/views.py
+++ browser/views.py
@@ -23,7 +23,7 @@ from engine.constants import *
 from http_handler.settings import WEBSITE, AWS_STORAGE_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 from registration.forms import RegistrationForm
 from schema.models import (FollowTag, ForwardingList, Group, MemberGroup, MemberGroupPending,
-                           MuteTag, Tag, UserProfile, Post, Attachment)
+                           MuteTag, Tag, UserProfile, Post, Attachment, DoNotSendList)
 from smtp_handler.utils import *
 
 request_error = json.dumps({'code': msg_code['REQUEST_ERROR'],'status':False})
