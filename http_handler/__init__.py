d6503dab743f7dc5cb9d33af85aa052446d4fb01
soyapark-kaist <soya@kaist.ac.kr>
2019-02-07 15:05:35 -0500

    Add to model folder

--- http_handler/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.schema.models import *
-from app.schema.youps import ImapAccount, MailbotMode,  Message_Thread, Message
-
-__all__ = ['UserProfile', 'ImapAccount', 'MailbotMode', 'Message_Thread', 'Message']
