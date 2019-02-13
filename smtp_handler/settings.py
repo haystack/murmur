b150561573b9b532ee7fd169fc069852d31ee626
soyapark-kaist <soya@mit.edu>
2019-02-12 20:50:34 -0500

    Celery init

---
+++ smtp_handler/settings.py
@@ -0,0 +1,6 @@
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
