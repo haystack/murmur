32e96218113457192e4454de3562a1854eb73074
soyapark <soya@mit.edu>
2019-02-17 15:55:19 -0500

    Can't be serialized

--- http_handler/tasks.py
+++ http_handler/tasks.py
@@ -7,10 +7,10 @@ from browser.imap import interpret
 logger = get_task_logger(__name__)
 
 # celery -A http_handler worker -l debug
-# celery -A http_handler beat --max-interval=10 -S djcelery.schedulers.DatabaseSchedule -l debug
+# celery -A http_handler beat --max-interval=10 -S djcelery.schedulers.DatabaseScheduler -l debug
 
 @task(name="add_periodic_task")
-def add_periodic_task(interval, imap_account, imap, code, search_creteria, is_test=False, email_content=None):
+def add_periodic_task(interval, imap_account_id, imap, code, search_creteria, is_test=False, email_content=None):
     """ create a new periodic task
 
     Args:
@@ -21,9 +21,10 @@ def add_periodic_task(interval, imap_account, imap, code, search_creteria, is_te
 
     # determine it is periodic or not 
     # callback to user profile to make sure we are not running out-dated code
-    print("ADD periodic task TASK performed!" + imap_account.email)
-    TaskScheduler.schedule_every('run_interpret', 'seconds', interval, [imap_account, imap, code, search_creteria, is_test, email_content])
-    return imap_account
+    print("ADD periodic task TASK performed!" + imap_account_id)
+    imapAccount = ImapAccount.objects.get(id=imap_account_id)
+    TaskScheduler.schedule_every('run_interpret', 'seconds', interval, [imapAccount, imap, code, search_creteria, is_test, email_content])
+    return imapAccount
 
 def remove_periodic_task():
     pass
