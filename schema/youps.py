a7519cfd4024819c1f1dc61a123b722b95153d59
Luke Murray <lukepigeonmail@gmail.com>
2019-02-28 13:22:06 -0500

    fix key length issue

--- schema/youps.py
+++ schema/youps.py
@@ -50,7 +50,8 @@ class FolderSchema(models.Model):
     # if this changes we have to invalidate our entire cache and refresh
     uid_validity = models.IntegerField(default=-1)
     # the name of the folder including it's entire path i.e. "work/project/youps"
-    name = models.TextField()
+    # TODO we need to determine the actual max length
+    name = models.TextField(max_length=1000)
     # the last seen uid which is helpful for reducing bandwith when syncing
     last_seen_uid = models.IntegerField(default=-1)
     # the flags associated with the folder 
