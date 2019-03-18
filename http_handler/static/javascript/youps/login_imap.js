d68be33893108300708e2d8c6fa921e59b4a54c5
soyapark <soya@mit.edu>
2019-03-18 00:05:33 -0400

    Datable collapsable

--- http_handler/static/javascript/youps/login_imap.js
+++ http_handler/static/javascript/youps/login_imap.js
@@ -283,7 +283,47 @@ $(document).ready(function() {
     // for demo; set date to now
     $(".current-date").text(format_date());
 
-    $('#example').DataTable();
+    /* Formatting function for row details - modify as you need */
+function format ( d ) {
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+       'Debugging result here..'+
+    '</table>';
+}
+
+var table = $('#example').DataTable( {
+    
+    // "columns": [
+    //     {
+    //         "className":      'details-control',
+    //         "orderable":      false,
+    //         "data":           null,
+    //         "defaultContent": ''
+    //     },
+    //     { "data": "sender" },
+    //     { "data": "subject" },
+    //     { "data": "deadline" },
+    //     { "data": "task" }
+    // ],
+    "order": [[1, 'asc']]
+} );
+ 
+// Add event listener for opening and closing details
+$('#example tbody').on('click', 'td.details-control', function () {
+    var tr = $(this).closest('tr');
+    var row = table.row( tr );
+
+    if ( row.child.isShown() ) {
+        // This row is already open - close it
+        row.child.hide();
+        tr.removeClass('shown');
+    }
+    else {
+        // Open this row
+        row.child( format(row.data()) ).show();
+        tr.addClass('shown');
+    }
+} );
 
     // Create the sandbox:
     // window.sandbox = new Sandbox.View({
