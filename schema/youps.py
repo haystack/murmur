bc0eba1ab33164c2ce3c2f33246434fec7f1b6d3
soyapark <soya@mit.edu>
2019-03-23 01:04:38 -0400

    Allow null for sender

--- schema/youps.py
+++ schema/youps.py
@@ -108,7 +108,7 @@ class MessageSchema(models.Model):
     # the contacts the message is from 
     from_ = models.ManyToManyField('ContactSchema', related_name='from_messages')
     # the contact the message was sent by, i.e. if a program sends a message for someone else
-    sender = models.ForeignKey('ContactSchema')
+    sender = models.ForeignKey('ContactSchema', null=True)
     # if a reply is sent it should be sent to these contacts
     reply_to = models.ManyToManyField('ContactSchema', related_name='reply_to_messages')
     # the contact the message is to
