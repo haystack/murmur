cd9e4ea529f59c2a31d60a072093983b22429c58
Raxel Gutierrez <raxelg@MacBook-Pro.local>
2020-07-16 23:09:15 -0400

    Added murmur dev server as ALLOWED_HOSTS

--- http_handler/settings.py
+++ http_handler/settings.py
@@ -103,6 +103,8 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ['murmur-dev.csail.mit.edu', 'localhost', '127.0.0.1']
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
