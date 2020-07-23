dbc38c000d6a3887737ca732f219c84d4a3c7cdc
Raxel Gutierrez <raxelg@MacBook-Pro.local>
2020-07-23 15:06:37 -0400

    Changed ALLOWED_HOSTS to dynamic BASE_URL

--- http_handler/settings.py
+++ http_handler/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ['murmur-dev.csail.mit.edu', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [BASE_URL]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
