3734462c25552c39fe8bfb81fb15b245db346c07
angcast <angcast@mit.edu>
2021-02-05 14:55:05 -0500

    changed comment in utilities.js

--- http_handler/static/javascript/utilities.js
+++ http_handler/static/javascript/utilities.js
@@ -1,7 +1,7 @@
 /* file used to keep functions that appear across multiple JS 
 files */
 
-// replaces spaces with underscores, replaces the replaceAll method in JS
+// replaces spaces with underscores, used as the replaceAll method in JS
 export function replaceSpace(group_name) {
     let friendly_name = '';
     for(let i = 0; i < group_name.length; i ++) {
