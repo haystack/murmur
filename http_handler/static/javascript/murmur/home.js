1aa1c66ec074b7d2eecce995b8be1ca0801229af
raxelg <raxelgutierre09@gmail.com>
2021-03-02 12:59:17 -0500

    Added sliding up animation for features section

--- http_handler/static/javascript/murmur/home.js
+++ http_handler/static/javascript/murmur/home.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 	console.log("HOME");
+
+	let options = {
+		root: null,
+		rootMargin: "0px",
+		threshold: 0.25,
+	}
+	
+	let slideIn = (entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				let elem = entry.target;
+
+				if (entry.intersectionRatio >= 0) {
+					elem.classList.add("slide-in");
+				}
+			}
+		});
+	};
+
+	let observer = new IntersectionObserver(slideIn, options);
+	let descTarget = document.querySelector("#feature-desc");
+	let demoTarget = document.querySelector("#feature-demo");
+	observer.observe(descTarget);
+	observer.observe(demoTarget);
 });
 
 
